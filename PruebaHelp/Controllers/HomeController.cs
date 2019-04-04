b78dd11611ffb9ea495190630c139107219ad20c
Leonardo Raul Melo Baez <20164056@itla.edu.do>
2019-04-04 15:30:29 -0400

    Si

---
+++ PruebaHelp/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using PruebaHelp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaHelp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Form()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Form(Datos persona)
+		{
+			
+
+			if (ModelState.IsValid) {
+
+				persona.cedula = Request.Form["cedula"].ToString();
+				persona.nombre = Request.Form["nombre"].ToString();
+				persona.apellido = Request.Form["apellido"].ToString();
+				persona.edad = Request.Form["edad"].ToString();
+				persona.telefono = Request.Form["telefono"].ToString();
+				persona.mail = Request.Form["mail"].ToString();
+				persona.genero = Request.Form["genero"].ToString();
+				persona.estado = Request.Form["estado"].ToString();
+
+				return RedirectToAction("Impresion", persona);
+				
+			}
+
+			return View(persona);
+		}
+	
+		public ActionResult Impresion(Datos persona)
+		{	
+			return View(persona);
+		}
+
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ PruebaHelp/Models/Datos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PruebaHelp.Models
+{
+	public class Datos
+	{
+
+		[Required(ErrorMessage = "Este campo es Obligatorio")]
+		public string cedula { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string nombre { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string apellido { get; set; }
+
+		[Range(15, 99)]
+		public string edad { get; set; }
+
+		public string telefono { get; set; }
+
+		[EmailAddress]
+		public string mail { get; set; }
+
+		public string genero { get; set; }
+
+		public string estado { get; set; }
+
+		public string hobbys { get; set; }
+	}
+}
